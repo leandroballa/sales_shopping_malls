81c48ea252288c7c186c2317244c32ca6b2133d1
Leandro Balla <leandro.balla@outlook.com>
2024-01-17 16:06:20 +1030

    First Commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+# Import data frames
+sales_df = pd.read_excel("datasets/Vendas.xlsx")
+sales_dez_df = pd.read_excel("datasets/Vendas - Dez.xlsx")
+managers_df = pd.read_excel("datasets/Gerentes.xlsx")
+
+# Adding Dezember Sales to Sales
+#sales_df = sales_df.append(sales_dez_df, ignore_index=True)
+sales_df = pd.concat([sales_df, sales_dez_df])
+
+# Total by Product Sale by Story
+sales_prod_store_df = sales_df[["ID Loja", "Produto", "Valor Final"]].groupby(["ID Loja", "Produto"]).sum().reset_index()
+
+# Total of Sales by Story
+sales_store_df = sales_prod_store_df[["ID Loja","Valor Final"]].groupby("ID Loja").sum().reset_index()
+
+# Adding Managers to Sales Store
+sales_store_df = sales_store_df.merge(managers_df)
+
+print(sales_store_df)
